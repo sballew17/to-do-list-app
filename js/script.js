48621b27719072eff3167791dd1e87eab8ef6681
sballew17 <samuel.ballew17@gmail.com>
2021-04-13 21:27:06 -0400

    Forgot ' '
    forgot the quotes for <li></li>

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function newItem(){
 
 //javascript
 //1. Adding a new item to the list of items:
-   let li = $(<li></li>);
+   let li = $('<li></li>'');
    let inputValue = $('#input').val();
    li.append(inputValue);
 
@@ -28,11 +28,9 @@ function newItem(){
  	 li.append(crossOutButton);
 
  	crossOutButton.on("click", deleteListItem);
- //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
    function deleteListItem(){
  		li.addClass("delete")
  	}
  // 4. Reordering the items:
    $('#list').sortable();
-
 }
