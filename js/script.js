f71147d3d60ac5614afc9530878528a0f87ac468
sballew17 <samuel.ballew17@gmail.com>
2021-04-13 21:29:36 -0400

    Forgot }
    forgot to close if element

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function newItem(){
      alert("You must write something!");
    } else {
      $('#list').append(li);
-
+}
  //2. Crossing out an item from the list of items:
    function crossOut() {
  		li.toggleClass("strike");
