d38f5452c39b0f1a2582b7f03469d2cf1f971ee3
sballew17 <samuel.ballew17@gmail.com>
2021-04-13 21:30:47 -0400

    extra '
    extra '

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function newItem(){
 
 //javascript
 //1. Adding a new item to the list of items:
-   let li = $('<li></li>'');
+   let li = $('<li></li>');
    let inputValue = $('#input').val();
    li.append(inputValue);
 
